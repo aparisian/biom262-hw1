9302bd39f3f7216d504bba6ef98650be02f8b6a9
Vivian Fu <vwfu@ucsd.edu>
2016-01-12 10:53:43 -0800

    hashtag

--- tf_binding.sh
+++ tf_binding.sh
@@ -27,6 +27,6 @@ bedtools flank -s -i gencode.v19.annotation.chr22.transcript.gtf -g hg19.genome 
 module load biotools
 bedtools intersect -a gencode.v19.annotation.chr22.transcript.promoter.gtf -b tf.nfkb.bed > gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf
 
-Exercise 5
+#Exercise 5
 module load biotools
 bedtools getfasta -s -fi GRCh37.p13.chr22.fa -bed gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf -fo gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta
