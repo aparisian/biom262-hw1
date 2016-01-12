f2c71357888cbf7a5641584fdc4521e71a86c625
Vivian Fu <vwfu@ucsd.edu>
2016-01-12 09:17:17 -0800

    exercises 4 and 5

--- tf_binding.sh
+++ tf_binding.sh
@@ -13,3 +13,10 @@ Hello I am a message in standard error(stderr) >&2
 
 echo "Hello I am a message in standard out (stdout)"
 
+Exercise 4
+module load biotools
+bedtools intersect -a gencode.v19.annotation.chr22.transcript.promoter.gtf -b tf.nfkb.bed > gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf
+
+Exercise 5
+module load biotools
+bedtools getfasta -s -fi GRCh37.p13.chr22.fa -bed gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf -fo gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta
