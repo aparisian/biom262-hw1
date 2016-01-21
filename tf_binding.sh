0bd0c61d2002ff3f683660c918e7e55a131a1cba
Alison Parisian <aparisian@ucsd.edu>
2016-01-21 11:41:54 -0800

    Added exercise 6

--- tf_binding.sh
+++ tf_binding.sh
@@ -30,3 +30,12 @@ bedtools intersect -a gencode.v19.annotation.chr22.transcript.promoter.gtf -b tf
 #Exercise 5
 module load biotools
 bedtools getfasta -s -fi GRCh37.p13.chr22.fa -bed gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf -fo gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta
+
+#Exercise 6
+#Promoter list doesn't correspond well with known NFKB binding sequences. 
+#Based on a comparison between the sequences in our generated file of promoters and the list of 11-mer canonical NFKB-binding sequences
+#from the paper, it seems that our generated list of promoters contains less than 10% of the known canonical NFKB-binding sequences
+#(only 283 out of 4400 probe sequences). This suggests that our list of NFKB-binding promoters is unlikely to be a complete list of
+#the regions where NFKB binds. In addition, a similar number of our promoters (332 out of 1221) contain a probe sequence.
+#This represents almost a quarter of the promoters in the list, but again suggests that either the list of probes or promoters is
+#likely to be incomplete.
